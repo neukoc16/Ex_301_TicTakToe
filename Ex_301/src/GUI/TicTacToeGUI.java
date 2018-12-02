c9908ae97170cb7c4ee3dd701a4fdec39bd2f9f1
neukoc16 <neukoc16@new.edvhtl.at>
2018-12-02 12:13:55 +0100

    minor changes

--- Ex_301/src/GUI/TicTacToeGUI.java
+++ Ex_301/src/GUI/TicTacToeGUI.java
@@ -1,25 +1,28 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class TicTacToeGUI extends JFrame {
-
+    
     public TicTacToeGUI() throws HeadlessException {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new GridLayout(3, 3));
         this.setSize(500, 500);
-
+        
         for (int i = 1; i <= 9; i++) {
-            JLabel label = new JLabel(i + "");
+            JLabel label = new JLabel();
+            label.setForeground(Color.RED);
+            label.setSize(10, 10);
             this.add(label);
-            label.add
         }
     }
-
+    
     public static void main(String[] args) {
-
+        new TicTacToeGUI().setVisible(true);
     }
 }
